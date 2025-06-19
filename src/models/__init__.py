82626209f311f925b4a92e1e64a50a0f886c5b1e
Mehul Pandita <106013240+panditamehul@users.noreply.github.com>
2025-06-19 17:43:17 +0530

    Delete src directory

--- src/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from .quantum_model import QuantumConvLayer, QuantumMRINet
-from .classical_model import ClassicalMRINet
-
-__all__ = [
-    'QuantumConvLayer',
-    'QuantumMRINet',
-    'ClassicalMRINet'
-]
